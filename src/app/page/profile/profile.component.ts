418f093a016c84ee6037ab1801c95e4c8248712b
Cristina Taboada <cris.taboada98@gmail.com>
2023-04-29 18:38:19 +0200

    Reto 2 Opcional completo

--- src/app/page/profile/profile.component.ts
+++ src/app/page/profile/profile.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { style } from '@angular/animations';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { User } from 'src/app/models/user';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-profile',
@@ -7,26 +9,44 @@ import { User } from 'src/app/models/user';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  @ViewChild('msgActu') changes: ElementRef;
+  @ViewChild('msgNoActu') noChanges: ElementRef;
+
   public profile: User;
 
-  constructor(){
+  constructor(private renderer2: Renderer2){
     this.profile = new User(1234,'Cristina','Taboada Mayo','cris.taboada98@gmail.com','../../../assets/img/foto-perfil.jpg','56789');
   }
 
   modify(name:string,last_name:string,email:string,photo:string){
-    console.log(this.profile);
+    const msgActu = this.changes.nativeElement;
+    const msgNoActu = this.noChanges.nativeElement;
+    let modify:boolean = false;
+
     if (name != '') {
       this.profile.name = name;
+      modify = true;
     }
     if (last_name != '') {
       this.profile.last_name = last_name;
+      modify = true;
     }
     if (email != '') {
       this.profile.email = email;
+      modify = true;
     }
     if (photo != '') {
       this.profile.photo = photo;
+      modify = true;
     }
-    console.log(this.profile);
+
+    if (modify == true) {
+      this.renderer2.setStyle(msgNoActu, 'display', 'none');
+      this.renderer2.setStyle(msgActu, 'display', 'block');
+      modify = false;
+    }else{
+      this.renderer2.setStyle(msgActu, 'display', 'none');
+      this.renderer2.setStyle(msgNoActu, 'display', 'block');
+    }
   }
 }
