ac22f5353a22a6e105967a31ce5d41861f688c17
Cristina Taboada <cris.taboada98@gmail.com>
2023-05-08 12:17:20 +0200

    Reto 3 completo

--- src/app/page/books/books.component.ts
+++ src/app/page/books/books.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from 'src/app/models/book';
 
 @Component({
   selector: 'app-books',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class BooksComponent {
 
+  public books: Book[];
+
+  constructor(){
+    this.books = [
+      new Book('Viuda de Hierro','Tapa blanda','Xiran Jay Zhao',18.05,'https://imagessl4.casadellibro.com/a/l/t7/04/9788427224704.jpg',982305,1234),
+      new Book('Ready Player One','Tapa blanda','Ernest Cline',15.00,'https://imagessl9.casadellibro.com/a/l/t7/79/9788466649179.jpg',743219,2345),
+      new Book('Trono de Cristal','Tapa blanda','Sarah J. Maas',14.50,'https://imagessl6.casadellibro.com/a/l/t7/86/9788418359286.jpg',561890,3456),
+      new Book('Harry Potter y la Piedra Filosofal','Tapa dura','J.K.Rowling',14.75,'https://imagessl2.casadellibro.com/a/l/t7/62/9788498382662.jpg',256734,4567),
+      new Book('Cuando los Árboles Cantan','Tapa blanda','Laura Gallego',12.95,'https://imagessl0.casadellibro.com/a/l/t7/30/9788467550030.jpg',629108,5678)
+    ]
+  }
 }
