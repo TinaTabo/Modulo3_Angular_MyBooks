6d0983f30a2edbf19352848031724145a6711efc
Cristina Taboada <cris.taboada98@gmail.com>
2023-05-15 13:52:28 +0200

    Buscador mejorado

--- src/app/page/books/books.component.ts
+++ src/app/page/books/books.component.ts
@@ -21,7 +21,19 @@ export class BooksComponent {
 
   searchBook(id_book:string):void{
     if (id_book != '') {
-      this.books = [this.booksService.getOne(Number(id_book))];
+      for (let i = 0; i < this.books.length; i++) {
+        if (Number(id_book) == this.books[i].id_book) {
+          this.books = [this.booksService.getOne(Number(id_book))];
+        }else{
+          let searchBookArray:Book[] = [];
+          for (const book of this.books) {
+            if (book.id_book.toString().indexOf(id_book) !== -1) {
+              searchBookArray.push(book);
+            }
+          }
+          this.books = searchBookArray;
+        }
+      }
     }else{
       this.books = this.booksService.getAll();
     }
