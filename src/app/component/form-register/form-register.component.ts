2390cc5aa14078db33a0e9641b4ba6d749d45afb
Cristina Taboada <cris.taboada98@gmail.com>
2023-05-19 16:18:30 +0200

    Reto 3 completo

--- src/app/component/form-register/form-register.component.ts
+++ src/app/component/form-register/form-register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl, ValidationErrors} from '@angular/forms';
 
 @Component({
   selector: 'app-form-register',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class FormRegisterComponent {
 
+  public form_register: FormGroup;
+
+  ngOnInit(){
+    let minLength:number = 8;
+    this.form_register = new FormGroup({
+      'name': new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      'lastname': new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(minLength)]),
+      'repeatPassword': new FormControl(null, [Validators.required, Validators.minLength(minLength)])
+    })
+  }
+
+  onSubmit(){
+    let name = this.form_register.get('name').value;
+    let lastname = this.form_register.get('lastname').value;
+    let email = this.form_register.get('email').value;
+    let password = this.form_register.get('password').value;
+    let repeatPassword = this.form_register.get('repeatPassword').value;
+
+    console.log(`Nombre: ${name}, Apellidos: ${lastname}`);
+    console.log(`Correo: ${email}`);
+    console.log(`Contraseña: ${password}, Verificación: ${repeatPassword}`);
+  }
 }
